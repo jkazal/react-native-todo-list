fe9a08257d673eb8333d3d38e71a6d5616e25a94
jkazal <johann.kazal@diiage.org>
2021-04-01 12:43:54 +0200

    feat(new task): add new task section

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [myLabel, setMyLabel] = useState('yoyoyoyoyoyo');
+  const addTask = () => console.log(myLabel.value);
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
       <View>
         <Text style={styles.titleSection}>Todo Today</Text>
         <View style={styles.tasksSection}>
@@ -14,7 +16,21 @@ export default function App() {
           <Task taskLabel="bibibi" />
         </View>
       </View>
-    </View>
+    <TextInput
+      value={myLabel}
+      style={styles.textinput}
+      onChangeText={(myLabel) => setMyLabel({ myLabel })}
+    ></TextInput>
+    <TouchableOpacity
+      onPress={() => {
+        addTask();
+      }}
+      >
+      <View style={styles.myButton}>
+        <Text>+</Text>
+      </View>
+    </TouchableOpacity>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -33,5 +49,18 @@ const styles = StyleSheet.create({
   tasksSection: {
     paddingTop: 80,
     paddingHorizontal: 20,
+  },
+  textinput: {
+    maxWidth: '80',
+    backgroundColor: 'white',
+    padding: '20px',
+    margin: '20px',
+    height: '20px'
+  },
+  myButton: {
+    backgroundColor: 'white',
+    borderRadius: '50%',
+    width: '50px',
+    height: '50px'
   }
 });
